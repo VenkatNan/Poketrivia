dd55fa1535cabacadc1e6cede1f849512aa39303
venkatnan <nanditavenkat@gmail.com>
2021-05-10 14:17:25 -0500

    fixed fetch

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hi");
+const url = "https://pokeapi.co/api/v2/pokemon/"
+
+
+    fetch(url + (3))
+        .then( res => res.json())
+        .then( parseJson => displayPoke(parseJson))
+
+function displayPoke(poke){
+    console.log(poke.species.name);
+    console.log(poke.sprites.front_default);
+}
+     
