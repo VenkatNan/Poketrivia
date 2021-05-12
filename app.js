000178da22d82ddc2bc337289c00beb135aca571
venkatnan <nanditavenkat@gmail.com>
2021-05-12 15:21:35 -0500

    re did fetch

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ const lvl1 = [1,2,3,4,5,6,7,8,9]
 const lvl2 = [10,11,12,13,14,15,16,17,18,19]
 const lvl3 = [101,102,103,104,105,106,107,108,109]
 
+const pokePic = document.getElementById("pic")
+const start = document.getElementById("play")
 
+start.addEventListener("click",getPoke())
 
 function setLvl(){
    getPoke(lvl1)
 }
 
-function getPoke(lvl){
-    fetch(url + (lvl[Math.floor(Math.random()*lvl.length)]))
+function getPoke(){
+    fetch(url + (Math.floor(Math.random()*898)))
         .then( res => res.json())
         .then( parseJson => displayPoke(parseJson))
 }
     
 function displayPoke(poke){
     console.log(poke.species.name);
-    console.log(poke.sprites.front_default);
+    console.log(pokePic.innerHTML);
+    pokePic.src = poke.sprites.front_default
 }
      
 
