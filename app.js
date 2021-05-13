bd9b76278d6c13271c26587997ab2a9ac395c2fb
venkatnan <nanditavenkat@gmail.com>
2021-05-13 11:09:09 -0500

    reload error found

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const url = "https://pokeapi.co/api/v2/pokemon/"
 
-let name= "";
+let pokeName= "";
+let points = 8;
 
 const pokePic = document.getElementById("pic");
 const play = document.getElementById("play");
 const info = document.getElementById("rules");
 const awnser = document.getElementById("btn");
 const input = document.getElementById("text");
+const pointCount = document.getElementById('points')
 
 info.addEventListener('click', function(){
     console.log("clicked info")
@@ -25,23 +27,36 @@ function getPoke(num){
 
 function displayPoke(poke){
     pokePic.src = poke.sprites.front_default;
-    let name = poke.species.name
-    console.log(name);
-
-    awnser.addEventListener('click', function checkAnswer(e) {
-        console.log(input.value);
-        if(input.value.toLowerCase() === name){
-            console.log("correct");
-            e.preventDefault();
-        }
-        else{
-            console.log("wrong");
-            e.preventDefault();
-        }
-        
-    })
+    pokeName = poke.species.name
+    console.log(pokeName);
 }
 
+awnser.addEventListener('click', function checkAnswer(e) {
+    console.log(input.value);
+    if(input.value.toLowerCase() === pokeName){
+        console.log("correct");
+        points ++
+        pointCount.innerText = points
+        checkWin()
+    }
+    else{
+        console.log(`${pokeName} is not ${input.value}`);
+        console.log("wrong");
+    }
+    e.preventDefault()
+})
+
+function checkWin() {
+    if(points = 10){
+        console.log(win);
+    }
+
+    else{
+        input.value = "";
+        getPoke(Math.floor(Math.random()*898))
+    }
+    Event.preventDefault()
+}
 
 
 
